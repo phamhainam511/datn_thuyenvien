87777d7472f1154ce87833794adbad6650ec7969
Nam <phamhainam511@gmail.com>
2025-05-30 02:30:28 +0700

    Update sửa chức vụ

--- src/services/ChucVuServices.js
+++ src/services/ChucVuServices.js
@@ -17,7 +17,6 @@ let createNewChucVu = async (data) => {
                     chungchi_id: chungchi_ids[i]
                 });
             }
-
             resolve(result);
         } catch (e) {
             reject(e);
@@ -58,14 +57,25 @@ let getChucVuId = (chucvu_id) => {
 let updateChucVuData = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            await db.Chucvu.update(
-                {
+            await db.Chucvu.update({
                     tenchucvu: data.tenchucvu,
                 },
                 {
                     where: { id_chucvu: data.id_chucvu }
-                }
-            );
+            });
+            await db.chucvuchungchi.destroy({
+                where: { chucvu_id: data.id_chucvu }
+            });
+            let chungchi_ids = data.chungchi_id || [];
+            if (!Array.isArray(chungchi_ids)) {
+                chungchi_ids = [chungchi_ids];
+            }
+            for (let i = 0; i < chungchi_ids.length; i++) {
+                await db.chucvuchungchi.create({
+                    chucvu_id: data.id_chucvu,
+                    chungchi_id: chungchi_ids[i]
+                });
+            }
             resolve('Cập nhật thành công!');
         } catch (e) {
             console.log(e);
@@ -100,11 +110,27 @@ let getComboChungChi = () => {
         }
     })
 }
+let getChungChiDaCo = (chucvu_id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let chungchis = db.chucvuchungchi.findAll({
+                attributes: ['chungchi_id'],
+                where:{
+                    chucvu_id: chucvu_id
+                }
+            });
+            resolve(chungchis);
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
 module.exports = {
     createNewChucVu: createNewChucVu,
     getAllChucVu: getAllChucVu,
     getChucVuId: getChucVuId,
     updateChucVuData: updateChucVuData,
     deleteChucVu: deleteChucVu,
-    getComboChungChi: getComboChungChi
+    getComboChungChi: getComboChungChi,
+    getChungChiDaCo: getChungChiDaCo
 }
