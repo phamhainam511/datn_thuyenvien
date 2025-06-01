e1fe0b01f4cd4582bc49b5ef794180dc2989263a
suadanghoccode <leilapham1123@gmail.com>
2025-06-01 22:17:14 +0700

    update import thui

--- src/services/ThuyenVienServices.js
+++ src/services/ThuyenVienServices.js
@@ -1,5 +1,5 @@
 import * as db from '../models/index.js';
-const dataUtils = require('./ChuanHoaServices.js');
+import dataUtils from './ChuanHoaServices.js';
 let createNewThuyenVien = async (data) => {
     return new Promise(async (resolve, reject) => {
         try {
